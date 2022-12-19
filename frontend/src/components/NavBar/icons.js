5020ff5cdd4baf679c3ba9a59003722e9d93bfd6
stanley <ou.stanley@yahoo.com>
2022-12-19 18:48:41 -0500

    Changed nav bar icons.

--- frontend/src/components/NavBar/icons.js
+++ frontend/src/components/NavBar/icons.js
@@ -33,18 +33,19 @@ const icons = {
     </svg>
   ),
   Home: (
-    <svg
-      aria-label="Home"
-      className="_ab6-"
-      color="#262626"
-      fill="#262626"
-      height="24"
-      role="img"
-      viewBox="0 0 24 24"
-      width="24"
-    >
-      <path d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z"></path>
-    </svg>
+    <i className="fa-solid fa-house fa-lg" />
+    // <svg
+    //   aria-label="Home"
+    //   className="_ab6-"
+    //   color="#262626"
+    //   fill="#262626"
+    //   height="24"
+    //   role="img"
+    //   viewBox="0 0 24 24"
+    //   width="24"
+    // >
+    //   <path d="M22 23h-6.001a1 1 0 0 1-1-1v-5.455a2.997 2.997 0 1 0-5.993 0V22a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V11.543a1.002 1.002 0 0 1 .31-.724l10-9.543a1.001 1.001 0 0 1 1.38 0l10 9.543a1.002 1.002 0 0 1 .31.724V22a1 1 0 0 1-1 1Z"></path>
+    // </svg>
   ),
   Search: (
     <svg
@@ -114,34 +115,35 @@ const icons = {
     </svg>
   ),
   Messages: (
-    <svg
-      aria-label="Direct"
-      className="_ab6-"
-      color="#262626"
-      fill="#262626"
-      height="24"
-      role="img"
-      viewBox="0 0 24 24"
-      width="24"
-    >
-      <line
-        fill="none"
-        stroke="currentColor"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        x1="22"
-        x2="9.218"
-        y1="3"
-        y2="10.083"
-      ></line>
-      <polygon
-        fill="none"
-        points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
-        stroke="currentColor"
-        strokeLinejoin="round"
-        strokeWidth="2"
-      ></polygon>
-    </svg>
+    <i className="fa-regular fa-comments fa-lg" />
+    // <svg
+    //   aria-label="Direct"
+    //   className="_ab6-"
+    //   color="#262626"
+    //   fill="#262626"
+    //   height="24"
+    //   role="img"
+    //   viewBox="0 0 24 24"
+    //   width="24"
+    // >
+    //   <line
+    //     fill="none"
+    //     stroke="currentColor"
+    //     strokeLinejoin="round"
+    //     strokeWidth="2"
+    //     x1="22"
+    //     x2="9.218"
+    //     y1="3"
+    //     y2="10.083"
+    //   ></line>
+    //   <polygon
+    //     fill="none"
+    //     points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
+    //     stroke="currentColor"
+    //     strokeLinejoin="round"
+    //     strokeWidth="2"
+    //   ></polygon>
+    // </svg>
   ),
   Notifications: (
     <svg
@@ -158,47 +160,48 @@ const icons = {
     </svg>
   ),
   Games: (
-    <svg
-      aria-label="New post"
-      className="_ab6-"
-      color="#262626"
-      fill="#262626"
-      height="24"
-      role="img"
-      viewBox="0 0 24 24"
-      width="24"
-    >
-      <path
-        d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552Z"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-      ></path>
-      <line
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        x1="6.545"
-        x2="17.455"
-        y1="12.001"
-        y2="12.001"
-      ></line>
-      <line
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        x1="12.003"
-        x2="12.003"
-        y1="6.545"
-        y2="17.455"
-      ></line>
-    </svg>
+    <i className="fa-solid fa-gamepad fa-lg" />
+    // <svg
+    //   aria-label="New post"
+    //   className="_ab6-"
+    //   color="#262626"
+    //   fill="#262626"
+    //   height="24"
+    //   role="img"
+    //   viewBox="0 0 24 24"
+    //   width="24"
+    // >
+    //   <path
+    //     d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552Z"
+    //     fill="none"
+    //     stroke="currentColor"
+    //     strokeLinecap="round"
+    //     strokeLinejoin="round"
+    //     strokeWidth="2"
+    //   ></path>
+    //   <line
+    //     fill="none"
+    //     stroke="currentColor"
+    //     strokeLinecap="round"
+    //     strokeLinejoin="round"
+    //     strokeWidth="2"
+    //     x1="6.545"
+    //     x2="17.455"
+    //     y1="12.001"
+    //     y2="12.001"
+    //   ></line>
+    //   <line
+    //     fill="none"
+    //     stroke="currentColor"
+    //     strokeLinecap="round"
+    //     strokeLinejoin="round"
+    //     strokeWidth="2"
+    //     x1="12.003"
+    //     x2="12.003"
+    //     y1="6.545"
+    //     y2="17.455"
+    //   ></line>
+    // </svg>
   ),
   More: (
     <svg
