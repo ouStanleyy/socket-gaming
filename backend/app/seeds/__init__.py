87174e19844634199ae376b3be6414f784a16b0f
stanley <ou.stanley@yahoo.com>
2023-01-05 18:25:56 -0500

    Updated files for deployment .

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -12,11 +12,20 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
-        # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
-        undo_users()
+        # # Before seeding in production, you want to run the seed undo
+        # # command, which will  truncate all tables prefixed with
+        # # the schema name (see comment in users.py undo_users function).
+        # # Make sure to add all your other model's undo functions below
+        # undo_users()
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.rooms RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.messages RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.games RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.players RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.occupants RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
     seed_users()
     # Add other seed functions here
 
